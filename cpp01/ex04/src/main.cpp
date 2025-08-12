b283e2ad00db33abddb69ab8e1325c9722e3483b
Julius Pellinen <jpelline@c1r2p1.hive.fi>
2025-08-12 18:32:53 +0300

    begun sed

--- cpp01/ex04/src/main.cpp
+++ cpp01/ex04/src/main.cpp
@@ -1,7 +1,46 @@
-#include <iostream>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpelline <jpelline@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/12 17:55:33 by jpelline          #+#    #+#             */
+/*   Updated: 2025/08/12 17:55:41 by jpelline         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int main()
+#include "sed.hpp"
+
+int main(int ac, char **av)
 {
-	std::cout << "hello world" << std::endl;
+	if (ac != 4) {
+		std::cerr << "error" << std::endl; return 0; }
+	std::string searchTerm  = av[2];
+	std::string replaceWith = av[3];
+	std::string file        = av[1];
+	std::ifstream input_stream(file);
+	if (!input_stream) std::cerr << "error with file";
+	std::string line;
+	while (getline(input_stream, line))
+	{
+		std::size_t found = line.find(searchTerm);
+		if (found == std::string::npos) {
+			std::cout << line << std::endl;
+			continue ;
+		}
+		line.erase(found, searchTerm.length());
+		line.insert(found, replaceWith);
+		std::cout << line << std::endl;
+		while (true)
+		{
+			found = line.find(searchTerm, found + 1);
+			if (found == std::string::npos)
+				break ;
+			line.erase(found, searchTerm.length());
+			line.insert(found, replaceWith);
+			std::cout << line << std::endl;
+		}
+	}
 	return 0;
 }
