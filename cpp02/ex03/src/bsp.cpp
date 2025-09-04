06ccee1788b8db6aca7921473de4043b66f3f24f
julius <julius.pellinen@gmail.com>
2025-09-04 14:01:14 +0300

    updated bsp

--- cpp02/ex03/src/bsp.cpp
+++ cpp02/ex03/src/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: jpelline <jpelline@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/21 12:45:06 by jpelline          #+#    #+#             */
-/*   Updated: 2025/09/04 14:00:47 by jpelline         ###   ########.fr       */
+/*   Updated: 2025/09/04 14:01:10 by jpelline         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,15 +37,15 @@ bool bsp(Point const a, Point const b, Point const c, Point const p) {
 
 	Fixed areaPAB = find_triangle_area(	p.get_x(), p.get_y(),
 										a.get_x(), a.get_y(),
-										b.get_x(), b.get_y());
+										b.get_x(), b.get_y() );
 
 	Fixed areaPAC = find_triangle_area(	p.get_x(), p.get_y(),
 										a.get_x(), a.get_y(),
-										c.get_x(), c.get_y());
+										c.get_x(), c.get_y() );
 
 	Fixed areaPBC = find_triangle_area(	p.get_x(), p.get_y(),
 										b.get_x(), b.get_y(),
-										c.get_x(), c.get_y());
+										c.get_x(), c.get_y() );
 
 	if (areaPAB == 0 || areaPAC == 0 || areaPBC == 0) return false;
 
