1d97c46caa381c85c827b275a13b8ec402f6f8ae
julius <julius.pellinen@gmail.com>
2025-09-04 14:00:52 +0300

    updated bsp

--- cpp02/ex03/src/bsp.cpp
+++ cpp02/ex03/src/bsp.cpp
@@ -6,13 +6,13 @@
 /*   By: jpelline <jpelline@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/21 12:45:06 by jpelline          #+#    #+#             */
-/*   Updated: 2025/09/04 13:56:29 by jpelline         ###   ########.fr       */
+/*   Updated: 2025/09/04 14:00:47 by jpelline         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-static Fixed triangle_area(const Fixed x1, const Fixed y1,
+static Fixed find_triangle_area(const Fixed x1, const Fixed y1,
 				  const Fixed x2, const Fixed y2,
 				  const Fixed x3, const Fixed y3) {
 
@@ -28,24 +28,24 @@ static Fixed triangle_area(const Fixed x1, const Fixed y1,
 	return area;
 }
 
-bool bsp(Point const a, Point const b, Point const c, Point const point) {
-	Fixed areaABC = triangle_area(	a.get_x(), a.get_y(),
-									b.get_x(), b.get_y(),
-									c.get_x(), c.get_y() );
+bool bsp(Point const a, Point const b, Point const c, Point const p) {
+	Fixed areaABC = find_triangle_area(	a.get_x(), a.get_y(),
+										b.get_x(), b.get_y(),
+										c.get_x(), c.get_y() );
 
 	if (areaABC == 0) return false;
 
-	Fixed areaPAB = triangle_area(	point.get_x(), point.get_y(),
-									a.get_x(), a.get_y(),
-									b.get_x(), b.get_y());
+	Fixed areaPAB = find_triangle_area(	p.get_x(), p.get_y(),
+										a.get_x(), a.get_y(),
+										b.get_x(), b.get_y());
 
-	Fixed areaPAC = triangle_area(	point.get_x(), point.get_y(),
-									a.get_x(), a.get_y(),
-									c.get_x(), c.get_y());
+	Fixed areaPAC = find_triangle_area(	p.get_x(), p.get_y(),
+										a.get_x(), a.get_y(),
+										c.get_x(), c.get_y());
 
-	Fixed areaPBC = triangle_area(	point.get_x(), point.get_y(),
-									b.get_x(), b.get_y(),
-									c.get_x(), c.get_y());
+	Fixed areaPBC = find_triangle_area(	p.get_x(), p.get_y(),
+										b.get_x(), b.get_y(),
+										c.get_x(), c.get_y());
 
 	if (areaPAB == 0 || areaPAC == 0 || areaPBC == 0) return false;
 
