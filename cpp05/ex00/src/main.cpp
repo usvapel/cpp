8ca2c9330addfc885720b16ed49091dd179456e1
julius <julius.pellinen@gmail.com>
2025-11-19 17:41:18 +0200

    update

--- cpp05/ex00/src/main.cpp
+++ cpp05/ex00/src/main.cpp
@@ -26,43 +26,41 @@ int main()
 
 	if (a.has_value()) {
 		std::cout << *a;
+		std::cout << a->getGrade() << std::endl;
+		try {
+			a->dec_grade();
+		} catch (const Bureaucrat::GradeTooLowException& e) {
+			std::cout << e.what() << '\n';
+		}
+		std::cout << a->getGrade() << std::endl << std::endl;
 	} else {
-		std::cout << "a not constructed" << '\n';
+		std::cout << a->getName() << " not constructed" << '\n';
 	}
-	
-	std::cout << a->getGrade() << std::endl;
-
-	try {
-		a->dec_grade();
-	} catch (const Bureaucrat::GradeTooLowException& e) {
-		std::cout << e.what() << '\n';
-	}
-
-	std::cout << a->getGrade() << std::endl << std::endl;
 
 	std::optional<Bureaucrat> b;
 
 	try {
-		b.emplace(1, "Bureucrat 2");
+		b.emplace(0, "Bureucrat 2");
 	} catch (const Bureaucrat::GradeTooHighException& e) {
+		b.reset();
 		std::cout << e.what() << '\n';
 	} catch (const Bureaucrat::GradeTooLowException& e) {
+		b.reset();
 		std::cout << e.what() << '\n';
 	}
 
 	if (b.has_value()) {
 		std::cout << *b;
+		std::cout << *b;
+		std::cout << b->getGrade() << std::endl;
+
+		try {
+			b->dec_grade();
+		} catch (const Bureaucrat::GradeTooHighException& e) {
+			std::cout << e.what() << '\n';
+		}
+		std::cout << b->getGrade() << std::endl << std::endl;
 	} else {
-		std::cout << "b not constructed" << '\n';
+		std::cout << b->getName() << " not constructed" << '\n';
 	}
-	
-	std::cout << b->getGrade() << std::endl;
-
-	try {
-		b->inc_grade();
-	} catch (const Bureaucrat::GradeTooHighException& e) {
-		std::cout << e.what() << '\n';
-	}
-
-	std::cout << b->getGrade() << std::endl << std::endl;
 }
