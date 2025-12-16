8caa077f99c24621c006f72417ff9690e4301e52
Usva Pellinen <julius.pellinen@gmail.com>
2025-12-16 17:09:36 +0200

    up

--- cpp08/ex02/include/MutantStack.hpp
+++ cpp08/ex02/include/MutantStack.hpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 
+template <typename T>
 class MutantStack {
 private:
 public:
@@ -9,12 +10,15 @@ public:
   MutantStack(const MutantStack &obj);
   MutantStack &operator=(const MutantStack &obj);
 
-  push();
-  pop();
-  top();
-  size();
-  begin();
-  end();
+  void push();
+  void pop();
+  void top();
+  int size();
+  int begin();
+  int end();
 
+  static T iterator(T val) {
+		return this->;
+	}
   ~MutantStack() = default;
 };
