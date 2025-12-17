951abe4488406ba96ccc2afc76b476b0c178657e
Usva Pellinen <julius.pellinen@gmail.com>
2025-12-17 20:44:05 +0200

    done

--- cpp08/ex02/include/MutantStack.hpp
+++ cpp08/ex02/include/MutantStack.hpp
@@ -1,24 +1,21 @@
 #pragma once
 
 #include <iostream>
+#include <stack>
 
-template <typename T>
-class MutantStack {
-private:
+template <typename T, class container = std::deque<T>>
+class MutantStack : public std::stack<T> {
 public:
-  MutantStack();
-  MutantStack(const MutantStack &obj);
-  MutantStack &operator=(const MutantStack &obj);
-
-  void push();
-  void pop();
-  void top();
-  int size();
-  int begin();
-  int end();
-
-  static T iterator(T val) {
-		return this->;
-	}
+  MutantStack() = default;
+  MutantStack(const MutantStack &obj) = default;
+  MutantStack &operator=(const MutantStack &obj) = default;
   ~MutantStack() = default;
+
+  typedef typename container::iterator iterator;
+  typedef typename container::const_iterator const_iterator;
+
+  iterator begin() { return this->c.begin(); };
+  iterator cbegin() { return this->c.cbegin(); };
+  iterator end() { return this->c.end(); };
+  iterator cend() { return this->c.cend(); };
 };
